c8df8fc804e7ace284c374e09eb44a3033cf114f
aldoprogrammer <107610584+aldoprogrammer@users.noreply.github.com>
2023-07-12 07:08:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// || is or and && is and
+
+
+let temp = -15;
+let sunny = true;
+
+if (temp > 0 || temp < 30  && sunny) {
+    console.log("The weather is good!");
+} else {
+    console.log("The weather is so freaking bad!")
+}
